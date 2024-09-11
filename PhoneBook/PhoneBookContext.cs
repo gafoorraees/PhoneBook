ee1f8f7a81eb1f7ce25c4d5fc0740fd5a3ecf664
gafoorraees <gafoorraees@gmail.com>
2024-09-11 02:03:17 -0400

    Add project files.

---
+++ PhoneBook/PhoneBookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Models;
+
+namespace PhoneBook;
+
+internal class PhonebookContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+       // optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+    }
+
+}
